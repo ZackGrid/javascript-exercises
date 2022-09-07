74d4f2be3dae295701e84f391ab2b5aeefd4deb0
ZackGrid <leautonac@gmail.com>
2022-09-07 11:38:47 -0300

    Add my solution for exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+//TOP solution, wich is much better than mine
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
+}
 
-};
+//My solution before seeing TOP solution file
+/*const reverseString = function(string) {
+    reversedString = "";
+    index = string.length - 1;
+    for (let i = 0; i < string.length; i++) {
+        reversedString += string.charAt(index);
+        index--;
+    }
+
+    return reversedString;
+};*/
 
 // Do not edit below this line
 module.exports = reverseString;
