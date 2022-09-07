0efd5bd27670aec6846040bab75716b2e7ed850b
ZackGrid <leautonac@gmail.com>
2022-09-07 10:57:34 -0300

    Add my solution for exercises 01/02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(words, number) {
+    let fullString = "";
+    for (let i = 0; i < number;i++){
+        fullString += words;
+    }
+    if (number < 0){
+        fullString = "ERROR";
+    }
+    return fullString;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
