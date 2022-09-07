9e96b6d84174fed625644fdc477602ba28faf025
ZackGrid <leautonac@gmail.com>
2022-09-07 17:03:01 -0300

    Add my solution for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+//My solution was quite similar to TOP solution
+const sumAll = function(x, y) {
+    let higher = 0;
+    let lower = 0;
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0 ) {
+        return "ERROR";       
+    } else if(x > y) {
+        higher = x;
+        lower = y;
+    } else {
+        higher = y;
+        lower = x;
+    }
+    let sum = lower;
+    for (let i = lower; i < higher; i++) {
+        sum += (i+1);
+    }
+    return sum;
 };
 
 // Do not edit below this line
