b1ec95d2e32d62d6119c816b26368805c393c70e
ZackGrid <leautonac@gmail.com>
2022-09-07 15:44:20 -0300

    Add my solution for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,28 @@
-const removeFromArray = function() {
 
+
+
+//My solution before seeing TOP solution
+/*
+const removeFromArray = function(array, ...args) {
+    for(let i = 0; i < args.length; i++) {
+        for(let j = 0; j < array.length; j++) {
+            if(args[i] === array[j]) {
+                array.splice(j, 1);
+            }
+        }
+    }
+    return array;
 };
+array.array.forEach(element => {
+    
+});
+*/
+
+//TOP solution
+const removeFromArray = function(...args) {
+    const array = args[0];
+    return array.filter(element => !args.includes(element))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
