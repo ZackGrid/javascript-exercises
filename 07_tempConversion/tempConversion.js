e61d2215ce545351ec595de7021b55a8689aea5e
ZackGrid <leautonac@gmail.com>
2022-09-08 10:31:03 -0300

    Add my solution for exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,24 @@
-const ftoc = function() {
-
+// my solution before seeing TOP solution
+const ftoc = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  return Number(celsius.toFixed(1));
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
+  let fahrenheit = celsius * (9 / 5) + 32;
+  return Number(fahrenheit.toFixed(1));
 
 };
+// TOP solution
+/*
+const ftoc = function(f) {
+  return Math.round((f - 32) * (5/9) * 10) / 10;
+};
 
+const ctof = function(c) {
+  return Math.round(((c * 9/5) + 32) * 10) / 10; 
+}
+*/
 // Do not edit below this line
 module.exports = {
   ftoc,
