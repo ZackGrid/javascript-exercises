b2d16f3f7c613130a134d750d917193d5ddc6b4f
ZackGrid <leautonac@gmail.com>
2022-09-08 09:42:50 -0300

    Add my solution for exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+// my solution before seeing TOP solution
+const leapYears = function(year) {
+    if(year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    } else if (year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
+// TOP solution
+/*
+const leapYears = function(year) {
+    return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
+}
+*/
 // Do not edit below this line
 module.exports = leapYears;
