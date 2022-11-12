b387a0eb9e596cf3e18462dea92d618c61cc1b5d
ZackGrid <leautonac@gmail.com>
2022-11-12 07:52:31 -0300

    Add my solution for exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {  
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  const total = a.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  const total = a.reduce((previousValue, currentValue) => previousValue * currentValue);
+  return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function (a) {
+  if (a === 0 || a === 1) {
+    return 1;
+  } else {
+    let fac = 1;
+    for (i = 1; i <= a; i++){
+      fac *= i;
+    }
+    return fac;
+  }
+    
+  };
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let temp1 = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+        .replace(/\s+/g, "")
+        .toLowerCase()
+    
+    let isPali = [...temp1];
+    let isPaliReverse = isPali.map(x => {
+        return x;
+    });
+    isPaliReverse.reverse();
+    let paliOne = isPali.toString();
+    let paliTwo = isPaliReverse.toString();
+    return paliOne === paliTwo;
 };
 
 // Do not edit below this line
