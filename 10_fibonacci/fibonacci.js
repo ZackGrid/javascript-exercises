af3be1b041e288a3cd7d4235fdefb07332ec870d
ZackGrid <leautonac@gmail.com>
2022-12-02 10:19:02 -0300

    Add my solution for exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (n < 0) {
+        return "OOPS";
+    } else {
+        let fiboArray = new Array(1, 1);
+        for (let i = 0; i < (n - 2); i++) {
+            let fibo = fiboArray[i] + fiboArray[i + 1];
+            fiboArray.push(fibo);
+            console.log(fibo);
+        }
+        return fiboArray[n - 1];
+    }
 
 };
 
