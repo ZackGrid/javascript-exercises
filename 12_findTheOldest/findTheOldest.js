46f3c163541ff97fc7c8f9819bf65655443bdd43
ZackGrid <leautonac@gmail.com>
2022-12-02 19:33:26 -0300

    Add my solution for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(personArray) {
+    const oldest = personArray.sort(function (a, b) {
+        const date = new Date().getFullYear();
+        const last = 'yearOfDeath' in a  ?
+            a.yearOfDeath - a.yearOfBirth :
+            date - a.yearOfBirth;
+            
+        const next = 'yearOfDeath' in b ?
+            b.yearOfDeath - b.yearOfBirth :
+            date - b.yearOfBirth;
+        
+        return last > next ? -1 : 1;
+        
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
